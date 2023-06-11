f3c0ba61a5acb38a4b6af3e88f06b99439527bfc
merill <merill@users.noreply.github.com>
2023-06-11 20:27:10 +1000

    Refactored

---
+++ src/documentgenerator/Infrastructure/ExcelTable.cs
@@ -0,0 +1,80 @@
+using Syncfusion.XlsIO;
+
+namespace ZeroTrustAssessment.DocumentGenerator.Infrastructure;
+
+public class ExcelTable
+{
+    private readonly IWorksheet _sheet;
+    private readonly string _rangeName;
+
+    private readonly int _startColumn;
+    private readonly int _startRow;
+    private int _currentRow;
+    private int _currentColumn;
+
+    public ExcelTable(IWorksheet sheet, string rangeName)
+    {
+        _sheet = sheet;
+        _rangeName = rangeName;
+
+        _startColumn = _sheet.Range[_rangeName].Column;
+        _startRow = _sheet.Range[_rangeName].Row;
+        _currentRow = _startRow;
+        _currentColumn = _startColumn;
+    }
+
+    public void AddColumn(string? value)
+    {
+        AddColumn(value, 1);
+    }
+    public void AddColumn(string? value, int columnWidth)
+    {
+        _sheet.Range[_currentRow, _currentColumn].Text = value;
+        if (columnWidth > 1) //Merge if needed
+        {
+            var range = $"{GetColumnName(_currentColumn)}{_currentRow}:{GetColumnName(_currentColumn + columnWidth - 1)}{_currentRow}";
+            _sheet.Range[range].Merge();
+        }
+        _currentColumn += columnWidth;
+    }
+
+    public void ShowNoDataMessage()
+    {
+        SetValue("Not configured");
+    }
+
+    private void SetValue(string value)
+    {
+        _sheet.Range[_rangeName].Text = value;
+    }
+
+    public void NextRow()
+    {
+        _currentRow++;
+        _currentColumn = _startColumn;
+    }
+
+    /// <summary>
+    /// Inserts blank rows into the sheet to prepare for data to be written.
+    /// </summary>
+    /// <param name="rowCount"></param>
+    public void InitializeRows(int rowCount)
+    {
+        _sheet.InsertRow(_startRow, rowCount);
+    }
+
+    private static string GetColumnName(int index)
+    {
+        index--; //Change to be zero based
+        const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        var value = "";
+
+        if (index >= letters.Length)
+            value += letters[index / letters.Length - 1];
+
+        value += letters[index % letters.Length];
+
+        return value;
+    }
+}
--- src/documentgenerator/Sheets/SheetConfigDevice.cs
+++ src/documentgenerator/Sheets/SheetConfigDevice.cs
@@ -20,27 +20,26 @@ public class SheetConfigDevice : SheetBase
 
     private void MdmWindowsEnrollment()
     {
+        var table = new ExcelTable(_sheet, "Table_WindowsEnrollment");
+
         if (_graphData.MobilityManagementPolicies == null)
         {
-            SetValue("Table_WindowsEnrollment", "Not configured");
+            table.ShowNoDataMessage();
             return;
         }
 
-        var startColumn = _sheet.Range["Table_WindowsEnrollment"].Column;
-        var startRow = _sheet.Range["Table_WindowsEnrollment"].Row;
-
-        var row = startRow;
         var rowCount = _graphData.MobilityManagementPolicies.Count;
-        _sheet.InsertRow(startRow, rowCount);
+        table.InitializeRows(rowCount);
         foreach (var policy in _graphData.MobilityManagementPolicies.OrderByDescending(x => x.AppliesTo).ThenBy(x => x.DisplayName))
         {
-            _sheet.SetText(row, startColumn, "MDM");
-            _sheet.SetText(row, startColumn + 1, policy.DisplayName);
-            _sheet.SetText(row, startColumn + 6, GetAppliesToName(policy.AppliesTo));
             var groupName = policy.AppliesTo == PolicyScope.Selected && policy.IncludedGroups != null ?
                 string.Join(", ", policy.IncludedGroups.Select(x => x.DisplayName)) : "N/A";
-            _sheet.SetText(row, startColumn + 8, groupName);
-            row++;
+
+            table.AddColumn("MDM");
+            table.AddColumn(policy.DisplayName, 5);
+            table.AddColumn(GetAppliesToName(policy.AppliesTo), 2);
+            table.AddColumn(groupName);
+            table.NextRow();
         }
     }
 
@@ -57,68 +56,83 @@ public class SheetConfigDevice : SheetBase
 
         var row = startRow;
         _sheet.InsertRow(startRow, rowCount);
-
-        if (platformRestrictions != null)
+        var views = EnrollmentRestrictionView.GetViews(_graphData);
+        foreach (var view in views)
         {
-            var restrictions = new Dictionary<string, DeviceEnrollmentPlatformRestrictionConfiguration>();
-            foreach (var config in platformRestrictions)
-            {
-                if (config is DeviceEnrollmentPlatformRestrictionConfiguration restriction) //First iterate and create list to make it easier to sort
-                {
-                    var sortKey = restriction.DisplayName + GetEnrollmentRestrictionPlatformTypeName(restriction.PlatformType) + restriction.Id;
-                    restrictions.Add(sortKey, restriction);
-                }
-            }
-            foreach (var item in restrictions.OrderBy(x => x.Key))
-            {
-                var restriction = item.Value;
-                var platform = GetEnrollmentRestrictionPlatformTypeName(restriction.PlatformType);
-                var priority = restriction.Priority;
-                var displayName = restriction.DisplayName;
-                var scopes = _graphData.GetScopesString(restriction.RoleScopeTagIds);
-                var assignments = _graphData.GetGroupAssignmentTargetText(restriction.Assignments);
+            _sheet.SetText(row, startColumn, view.Platform);
+            _sheet.SetValue(row, startColumn + 3, $"{view.Priority}");
+            _sheet.SetText(row, startColumn + 4, view.DisplayName);
 
-                AddDeviceEnrollmentRow(row, startColumn, platform, priority, displayName, restriction.PlatformRestriction, scopes, assignments);
-
-                row++;
-            }
+            _sheet.SetText(row, startColumn + 8, view.PlatformBlocked);
+            _sheet.SetText(row, startColumn + 9, view.OsMinimumVersion);
+            _sheet.SetText(row, startColumn + 10, view.OsMaximumVersion);
+            _sheet.SetText(row, startColumn + 11, view.PersonalDeviceEnrollmentBlocked);
+            _sheet.SetText(row, startColumn + 13, view.BlockedManufacturers);
+            _sheet.SetText(row, startColumn + 15, view.Scopes);
+            _sheet.SetText(row, startColumn + 16, view.Assignments);
+            row++;
         }
-        var defaultPlatformRestrictions = _graphData.DeviceEnrollmentConfigurations?
-            .Where(x => x.DeviceEnrollmentConfigurationType == DeviceEnrollmentConfigurationType.PlatformRestrictions).FirstOrDefault();
-        if (defaultPlatformRestrictions != null && defaultPlatformRestrictions is DeviceEnrollmentPlatformRestrictionsConfiguration defaultRestriction)
-        {
+        // if (platformRestrictions != null)
+        // {
+        //     var restrictions = new Dictionary<string, DeviceEnrollmentPlatformRestrictionConfiguration>();
+        //     foreach (var config in platformRestrictions)
+        //     {
+        //         if (config is DeviceEnrollmentPlatformRestrictionConfiguration restriction) //First iterate and create list to make it easier to sort
+        //         {
+        //             var sortKey = restriction.DisplayName + GetEnrollmentRestrictionPlatformTypeName(restriction.PlatformType) + restriction.Id;
+        //             restrictions.Add(sortKey, restriction);
+        //         }
+        //     }
+        //     foreach (var item in restrictions.OrderBy(x => x.Key))
+        //     {
+        //         var restriction = item.Value;
+        //         var platform = GetEnrollmentRestrictionPlatformTypeName(restriction.PlatformType);
+        //         var priority = restriction.Priority;
+        //         var displayName = restriction.DisplayName;
+        //         var scopes = _graphData.GetScopesString(restriction.RoleScopeTagIds);
+        //         var assignments = _graphData.GetGroupAssignmentTargetText(restriction.Assignments);
 
-            var platform = string.Empty;
-            var priority = defaultRestriction.Priority;
-            var displayName = "Default";
-            var scopes = _graphData.GetScopesString(defaultRestriction.RoleScopeTagIds);
-            var assignments = "All users and all devices";
+        //         AddDeviceEnrollmentRow(row, startColumn, platform, priority, displayName, restriction.PlatformRestriction, scopes, assignments);
 
-            AddDeviceEnrollmentRow(row, startColumn, Labels.PlatformAndroidForWork, priority, displayName, defaultRestriction.AndroidForWorkRestriction, scopes, assignments); row++;
-            AddDeviceEnrollmentRow(row, startColumn, Labels.PlatformAndroid, priority, displayName, defaultRestriction.AndroidRestriction, scopes, assignments); row++;
-            AddDeviceEnrollmentRow(row, startColumn, Labels.PlatformIos, priority, displayName, defaultRestriction.IosRestriction, scopes, assignments); row++;
-            AddDeviceEnrollmentRow(row, startColumn, Labels.PlatformMacOs, priority, displayName, defaultRestriction.MacOSRestriction, scopes, assignments); row++;
-            AddDeviceEnrollmentRow(row, startColumn, Labels.PlatformWindows, priority, displayName, defaultRestriction.WindowsRestriction, scopes, assignments); row++;
-        }
+        //         row++;
+        //     }
+        // }
+        // var defaultPlatformRestrictions = _graphData.DeviceEnrollmentConfigurations?
+        //     .Where(x => x.DeviceEnrollmentConfigurationType == DeviceEnrollmentConfigurationType.PlatformRestrictions).FirstOrDefault();
+        // if (defaultPlatformRestrictions != null && defaultPlatformRestrictions is DeviceEnrollmentPlatformRestrictionsConfiguration defaultRestriction)
+        // {
+
+        //     var platform = string.Empty;
+        //     var priority = defaultRestriction.Priority;
+        //     var displayName = "Default";
+        //     var scopes = _graphData.GetScopesString(defaultRestriction.RoleScopeTagIds);
+        //     var assignments = "All users and all devices";
+
+        //     AddDeviceEnrollmentRow(row, startColumn, Labels.PlatformAndroidForWork, priority, displayName, defaultRestriction.AndroidForWorkRestriction, scopes, assignments); row++;
+        //     AddDeviceEnrollmentRow(row, startColumn, Labels.PlatformAndroid, priority, displayName, defaultRestriction.AndroidRestriction, scopes, assignments); row++;
+        //     AddDeviceEnrollmentRow(row, startColumn, Labels.PlatformIos, priority, displayName, defaultRestriction.IosRestriction, scopes, assignments); row++;
+        //     AddDeviceEnrollmentRow(row, startColumn, Labels.PlatformMacOs, priority, displayName, defaultRestriction.MacOSRestriction, scopes, assignments); row++;
+        //     AddDeviceEnrollmentRow(row, startColumn, Labels.PlatformWindows, priority, displayName, defaultRestriction.WindowsRestriction, scopes, assignments); row++;
+        // }
 
     }
-    private void AddDeviceEnrollmentRow(int row, int startColumn, string platform, int? priority, string? displayName, DeviceEnrollmentPlatformRestriction? platformRestriction, string scopes, string assignments)
-    {
-        _sheet.SetText(row, startColumn, platform);
-        _sheet.SetValue(row, startColumn + 3, $"{priority}");
-        _sheet.SetText(row, startColumn + 4, displayName);
+    // private void AddDeviceEnrollmentRow(int row, int startColumn, string platform, int? priority, string? displayName, DeviceEnrollmentPlatformRestriction? platformRestriction, string scopes, string assignments)
+    // {
+    //     _sheet.SetText(row, startColumn, platform);
+    //     _sheet.SetValue(row, startColumn + 3, $"{priority}");
+    //     _sheet.SetText(row, startColumn + 4, displayName);
 
-        if (platformRestriction != null)
-        {
-            _sheet.SetText(row, startColumn + 8, GetIsBlockedString(platformRestriction.PlatformBlocked));
-            _sheet.SetText(row, startColumn + 9, GetString(platformRestriction.OsMinimumVersion));
-            _sheet.SetText(row, startColumn + 10, GetString(platformRestriction.OsMaximumVersion));
-            _sheet.SetText(row, startColumn + 11, GetIsBlockedString(platformRestriction.PersonalDeviceEnrollmentBlocked));
-            _sheet.SetText(row, startColumn + 13, platformRestriction.BlockedManufacturers?.Count > 0 ? string.Join(", ", platformRestriction.BlockedManufacturers) : string.Empty);
-            _sheet.SetText(row, startColumn + 15, scopes);
-            _sheet.SetText(row, startColumn + 16, assignments);
-        }
-    }
+    //     if (platformRestriction != null)
+    //     {
+    //         _sheet.SetText(row, startColumn + 8, GetIsBlockedString(platformRestriction.PlatformBlocked));
+    //         _sheet.SetText(row, startColumn + 9, GetString(platformRestriction.OsMinimumVersion));
+    //         _sheet.SetText(row, startColumn + 10, GetString(platformRestriction.OsMaximumVersion));
+    //         _sheet.SetText(row, startColumn + 11, GetIsBlockedString(platformRestriction.PersonalDeviceEnrollmentBlocked));
+    //         _sheet.SetText(row, startColumn + 13, platformRestriction.BlockedManufacturers?.Count > 0 ? string.Join(", ", platformRestriction.BlockedManufacturers) : string.Empty);
+    //         _sheet.SetText(row, startColumn + 15, scopes);
+    //         _sheet.SetText(row, startColumn + 16, assignments);
+    //     }
+    // }
 
 
     private void DeviceCompliancePolicies()
@@ -183,12 +197,12 @@ public class SheetConfigDevice : SheetBase
     }
 
 
-    private string GetString(string? value)
+    public static string GetString(string? value)
     {
         return value ?? string.Empty;
     }
 
-    private static string GetAppliesToName(PolicyScope? scope)
+    public static string GetAppliesToName(PolicyScope? scope)
     {
         if (scope == null) return string.Empty;
 
@@ -200,23 +214,7 @@ public class SheetConfigDevice : SheetBase
             _ => nameof(scope),
         };
     }
-
-    private static string GetEnrollmentRestrictionPlatformTypeName(EnrollmentRestrictionPlatformType? platformType)
-    {
-        if (platformType == null) return string.Empty;
-
-        return platformType switch
-        {
-            EnrollmentRestrictionPlatformType.Android => Labels.PlatformAndroid,
-            EnrollmentRestrictionPlatformType.AndroidForWork => Labels.PlatformAndroidForWork,
-            EnrollmentRestrictionPlatformType.Windows => Labels.PlatformWindows,
-            EnrollmentRestrictionPlatformType.Ios => Labels.PlatformIos,
-            EnrollmentRestrictionPlatformType.Mac => Labels.PlatformMacOs,
-            _ => nameof(platformType),
-        };
-    }
-
-    private static string GetIsBlockedString(bool? value)
+    public static string GetIsBlockedString(bool? value)
     {
         if (value == null) return string.Empty;
         return (bool)value ? "Blocked" : "Allowed";
