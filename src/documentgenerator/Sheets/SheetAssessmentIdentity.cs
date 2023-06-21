a35165b253b76eafbe454ffc126afa7af12c27c1
merill <merill@users.noreply.github.com>
2023-06-21 22:53:52 +1000

    Added security info

--- src/documentgenerator/Sheets/SheetAssessmentIdentity.cs
+++ src/documentgenerator/Sheets/SheetAssessmentIdentity.cs
@@ -14,6 +14,8 @@ public class SheetAssessmentIdentity : SheetBase
         WorkloadChecks();
         TenantAppManagementPolicy();
         GlobalAdminPhishingResistantAuthStrength();
+        BasicMfaUsageInsteadOfAuthStrength();
+        SecureRegistrationOfSecurityInfo();
     }
 
     private void WorkloadChecks()
@@ -43,8 +45,40 @@ public class SheetAssessmentIdentity : SheetBase
             x => x?.Conditions?.Users?.IncludeRoles?.Contains(globalAdminRoleId) == true &&
                 x?.GrantControls?.AuthenticationStrength != null);
 
-
         var result = globalAdminPoliciesWithMfa ? AssessmentValue.Completed : AssessmentValue.NotStartedP1;
         SetValue("I00003_GlobalAdminPhishingResistantAuthStrength", result);
+    }
+
+    private void BasicMfaUsageInsteadOfAuthStrength()
+    {
+        var basicMfaInUse = _graphData.ConditionalAccessPolicies.Any(
+            x => x?.GrantControls?.BuiltInControls?.Contains(ConditionalAccessGrantControl.Mfa) == true);
+
+        var resultNoAuthStrength = basicMfaInUse ? AssessmentValue.NotStartedP2 : AssessmentValue.Completed;
+
+        var hasAuthStrength = _graphData.ConditionalAccessPolicies.Any(
+            x =>  x?.GrantControls?.AuthenticationStrength != null);
+
+        var resultBasicMfa = basicMfaInUse || hasAuthStrength ? AssessmentValue.Completed : AssessmentValue.Completed;
+
+        SetValue("I00004_BasicMfaUsageInsteadOfAuthStrength", resultNoAuthStrength);
+        SetValue("I00005_BasicMfaInUse", resultBasicMfa);
+    }
+
+    private void SecureRegistrationOfSecurityInfo()
+    {
+        var hasSecurityInfo = _graphData.ConditionalAccessPolicies.Any(
+            x => x?.Conditions?.Applications?.IncludeUserActions?.Contains("urn:user:registersecurityinfo") == true);
+
+        var hasSecurityInfoAndDevice = _graphData.ConditionalAccessPolicies.Any(
+            x => x?.Conditions?.Applications?.IncludeUserActions?.Contains("urn:user:registersecurityinfo") == true &&
+            (x?.GrantControls?.BuiltInControls?.Contains(ConditionalAccessGrantControl.CompliantDevice) == true ||
+            x?.GrantControls?.BuiltInControls?.Contains(ConditionalAccessGrantControl.DomainJoinedDevice) == true )
+            );
+
+        var result = hasSecurityInfoAndDevice ? AssessmentValue.Completed : 
+                       hasSecurityInfo ? AssessmentValue.NotStartedP1 : AssessmentValue.NotStartedP0;
+
+        SetValue("I00006_SecureRegistrationOfSecurityInfo", result);
     }
 }
