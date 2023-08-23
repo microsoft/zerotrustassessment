e3394a4fe0f95ad5b1f12c68a482de95b63a7fae
merill <merill@users.noreply.github.com>
2023-08-23 13:53:57 +1000

    Update InvokeZtAssessmentCmdlet.cs

--- src/powershell/ZeroTrustAssessment/InvokeZtAssessmentCmdlet.cs
+++ src/powershell/ZeroTrustAssessment/InvokeZtAssessmentCmdlet.cs
@@ -13,13 +13,13 @@ public class InvokeZtAssessmentCmdletCommand : PSCmdlet
         Position = 0,
         ValueFromPipeline = true,
         ValueFromPipelineByPropertyName = true)]
-    public int FavoriteNumber { get; set; }
+    public string Path { get; set; }
 
     [Parameter(
+        Mandatory = true,
         Position = 1,
         ValueFromPipelineByPropertyName = true)]
-    [ValidateSet("Cat", "Dog", "Horse")]
-    public string FavoritePet { get; set; } = "Dog";
+    public string AccessToken { get; set; }
 
     // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
     protected override void BeginProcessing()
@@ -30,10 +30,7 @@ public class InvokeZtAssessmentCmdletCommand : PSCmdlet
     // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
     protected override void ProcessRecord()
     {
-        WriteObject(new FavoriteStuff { 
-            FavoriteNumber = FavoriteNumber,
-            FavoritePet = FavoritePet
-        });
+        WriteObject("Exporting to ");
     }
 
     // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
