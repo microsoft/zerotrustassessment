d0ec3e7c6ef212729e7705c431a3672ee4750f88
merill <merill@users.noreply.github.com>
2023-12-13 13:21:30 +1100

    Restored automated build

--- src/react/docusaurus.config.js
+++ src/react/docusaurus.config.js
@@ -13,14 +13,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://merill.net',
+  url: 'https://microsoft.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/zerotrustassessment/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'merill', // Usually your GitHub org/user name.
+  organizationName: 'microsoft', // Usually your GitHub org/user name.
   projectName: 'zerotrustassessment', // Usually your repo name.
   trailingSlash: false,
 
