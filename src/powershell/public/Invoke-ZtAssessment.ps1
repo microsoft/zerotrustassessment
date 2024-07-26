08ca58f4d9738cfbdc30c2615c374a890618b754
Merill Fernando <merill@merill.net>
2024-07-27 08:27:19 +1000

    Added console output

--- src/powershell/public/Invoke-ZtAssessment.ps1
+++ src/powershell/public/Invoke-ZtAssessment.ps1
@@ -21,11 +21,26 @@ function Invoke-ZtAssessment
         $Path = "./ZeroTrustReport"
     )
 
+    $banner = @"
++-------------------------------------------------------------+
+| Microsoft Zero Trust Assessment v1.0                        |
++-------------------------------------------------------------+
+"@
+    Write-Host $banner -ForegroundColor Cyan
+
     # Stop if folder has items inside it
     if (Test-Path $Path) {
         if ((Get-ChildItem $Path).Count -gt 0) {
-            Write-Error "Folder $Path is not empty. Please provide a path to an empty folder."
-            return
+            # Prompt user if it's okay to delete the folder and get confirmation
+            Write-Host "`nFolder $Path is not empty. Do you want to delete the contents and continue (y/n)?" -ForegroundColor Yellow -NoNewline
+            $deleteFolder = Read-Host
+            if ($deleteFolder -eq "y") {
+                Remove-Item -Path $Path -Recurse -Force -ErrorAction Stop | Out-Null
+            }
+            else {
+                Write-Error "Folder $Path is not empty. Please provide a path to an empty folder."
+                return
+            }
         }
     }
 
@@ -33,7 +48,7 @@ function Invoke-ZtAssessment
 
     Clear-ZtModuleVariable # Reset the graph cache and urls to avoid stale data
 
-    Write-Verbose 'Creating report folder $OutputFolder'
+    Write-Verbose 'Creating report folder $Path'
     New-Item -ItemType Directory -Path $Path -ErrorAction Stop | Out-Null
 
     # Collect data
@@ -51,7 +66,8 @@ function Invoke-ZtAssessment
     $output = Get-HtmlReport -AssessmentResults $assessmentResultsJson
     $output | Out-File -FilePath $htmlReportPath -Encoding UTF8
 
+    Write-Host
     Write-Host "🛡️ Zero Trust Assessmet report generated at $htmlReportPath" -ForegroundColor Green
-
+    Write-Host
     Invoke-Item $htmlReportPath | Out-Null
 }
