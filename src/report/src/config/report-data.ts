db45b70eae9842a96d85d81985410bf49eba82c5
Merill Fernando <merill@merill.net>
2024-10-29 14:45:00 +1100

    Update report-data.ts

--- src/report/src/config/report-data.ts
+++ src/report/src/config/report-data.ts
@@ -39,7 +39,7 @@ export interface Test {
 }
 
 export const reportData: ZeroTrustAssessmentReport = {
-  "ExecutedAt": "2024-09-12T19:39:37.667906+10:00",
+  "ExecutedAt": "2024-09-16T20:55:42.757028+10:00",
   "TenantId": "0817c655-a853-4d8f-9723-3a333b5b9235",
   "TenantName": "Pora",
   "Domain": "elapora.com",
@@ -48,24 +48,47 @@ export const reportData: ZeroTrustAssessmentReport = {
   "LatestVersion": "1.0.2",
   "Tests": [
     {
-      "TestStatus": "Passed",
-      "TestDescription": " Attackers might exploit valid but dormant applications that still have high privileges. These applications can be used to gain initial access without raising alarm because they're legitimate applications. From there, attackers can use the application privileges to plan or execute other attacks. Attackers might also maintain access by manipulating the dormant application, such as by adding credentials. This persistence ensures that even if their primary access method is detected, they can regain access later.\n\n#### Remediation\n\n- Disable privileged Service Principals  \n  - [Update serviceprincipal - Microsoft Graph v1.0](https://learn.microsoft.com/graph/api/serviceprincipal-update?view=graph-rest-1.0&tabs=http)\n- Investigate if the application has legitimate use cases\n- If service principal does not have  legitimate usage, delete it\n  - [Delete servicePrincipal - Microsoft Graph v1.0](https://learn.microsoft.com/graph/api/serviceprincipal-delete?view=graph-rest-1.0&tabs=http)\n\n",
-      "TestId": "21770",
+      "TestStatus": "Failed",
+      "TestDescription": "Certificates, if not securely stored, can be extracted and exploited by attackers, leading to unauthorized access. Long-lived certificates are more likely to be exposed over time. Credentials, when exposed, provide attackers with legitimate credentials to blend their activities with legitimate operations, making it easier to bypass security controls. If an attacker compromises an application’s certificate, they can escalate their privileges within the system, leading to broader access and control, depending on the privileges of the application.\n\n#### Remediation\n\n- [Define certificate based application configuration](https://devblogs.microsoft.com/identity/app-management-policy/)\n- [certificateBasedApplicationConfiguration](https://learn.microsoft.com/graph/api/resources/certificatebasedapplicationconfiguration)\n- [Create a the least-privileged custom role to rotate application credentials](https://learn.microsoft.com/entra/identity/role-based-access-control/custom-create) \n\n",
+      "TestId": "21773",
       "TestRisk": "High",
-      "TestTitle": "Inactive applications don't have highly privileged permissions",
+      "TestTitle": "Applications don't have certificates with expiration longer than 180 days",
       "TestAppliesTo": [
         "Entra"
       ],
       "TestSkipped": "",
-      "TestResult": "\nNo inactive applications with high privileges\n",
+      "TestResult": "\nThe following applications and service principals have certificates longer than 180 days\n\n\n## Applications with long-lived credentials\n\n| Application | Certificate Expiry |\n| :--- | :--- |\n| [PnPPowerShell](https://entra.microsoft.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Credentials/appId/1d93462e-0f39-4e4c-898a-b6b1df5fa997) | 03/28/2031 13:00:00 |\n| [SharePoint Version App](https://entra.microsoft.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Credentials/appId/2bb68591-782c-4c64-9415-bdf9414ae400) | 11/16/2033 13:00:00 |\n\n\n## Service Principals with long-lived credentials\n\n| Service Principal | Tenant | Certificate Expiry |\n| :--- | :--- | :--- |\n| [AWS Single-Account Access](https://entra.microsoft.com/#view/Microsoft_AAD_IAM/ManagedAppMenuBlade/~/SignOn/objectId/dc89bf5d-83e8-4419-9162-3b9280a85755/appId/091edd89-b342-4bb5-9144-82fe6c913987/preferredSingleSignOnMode/saml/servicePrincipalType/Application/fromNav/) | Pora | 10/26/2025 |\n| [Saml test entity id](https://entra.microsoft.com/#view/Microsoft_AAD_IAM/ManagedAppMenuBlade/~/SignOn/objectId/d589a2e6-4a78-4cdd-901b-f574dc7880db/appId/6590313e-1c00-4c07-be28-72858e837a52/preferredSingleSignOnMode/saml/servicePrincipalType/Application/fromNav/) | Pora | 11/17/2025 |\n| [SumoLogic](https://entra.microsoft.com/#view/Microsoft_AAD_IAM/ManagedAppMenuBlade/~/SignOn/objectId/144df581-a0c9-4280-9739-0c3612ab9ccf/appId/1b6c36b2-f2b1-43d3-b74f-8d52c44fc9c1/preferredSingleSignOnMode/saml/servicePrincipalType/Application/fromNav/) | Pora | 02/15/2027 |\n| [test](https://entra.microsoft.com/#view/Microsoft_AAD_IAM/ManagedAppMenuBlade/~/SignOn/objectId/31e80a1b-3faa-4ce9-9794-2b77f61f20f7/appId/8ae2b566-71f5-467e-8960-cfe8da3a2cfa/preferredSingleSignOnMode/saml/servicePrincipalType/Application/fromNav/) | Pora | 10/10/2025 |\n\n",
       "TestTags": [
         "Application"
       ],
-      "TestImpact": "High",
-      "TestImplementationCost": "Low",
+      "TestImpact": null,
+      "TestImplementationCost": "Medium",
       "SkippedReason": null
     }
   ],
-  "TenantInfo": {},
+  "TenantInfo": {
+    "OverviewCaMfaAllUsers": [
+      {
+        "value": 1,
+        "target": "No CA applied",
+        "source": "User sign in"
+      },
+      {
+        "value": 1244,
+        "target": "CA applied",
+        "source": "User sign in"
+      },
+      {
+        "value": 0,
+        "target": "No MFA",
+        "source": "CA applied"
+      },
+      {
+        "value": 1244,
+        "target": "MFA",
+        "source": "CA applied"
+      }
+    ]
+  },
   "EndOfJson": "EndOfJson"
 }
