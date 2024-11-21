0541c727515e16f1024d0b2fbd501d8d25afecc8
merill <merill@users.noreply.github.com>
2024-11-22 00:07:27 +1100

    21782

--- src/powershell/private/tests/Test-PrivilegedUsersPhishResistantMethodRegistered.21782.ps1
+++ src/powershell/private/tests/Test-PrivilegedUsersPhishResistantMethodRegistered.21782.ps1
@@ -14,92 +14,60 @@ function Test-PrivilegedUsersPhishResistantMethodRegistered {
     Write-PSFMessage '🟦 Start' -Tag Test -Level VeryVerbose
 
     $activity = "Checking phishing resistant authentication for privileged roles"
-    Write-ZtProgress -Activity $activity -Status "Getting policy"
+    Write-ZtProgress -Activity $activity -Status "Getting authentication methods"
 
-    # TODO: Check for report-only and exclude from pass state. Include CA state in the CA outpu
-    # -Include a pass / partial / fail next to each CA policy to show which ones have phish resistant for roles.
+    $sql = @"
+select distinct id, userDisplayName, roleDisplayName, methodsRegistered, list_has_any(['passKeyDeviceBound', 'passKeyDeviceBoundAuthenticator', 'windowsHelloForBusiness'], methodsRegistered) as phishResistantAuthMethod
+from UserRegistrationDetails u
+    inner join vwRole r on u.id = r.principalId
+"@
+    $results = Invoke-DatabaseQuery -Database $Database -Sql $sql
 
-    $roles = Invoke-ZtGraphRequest -RelativeUri 'roleManagement/directory/roleDefinitions' -ApiVersion beta
-    $caps = Invoke-ZtGraphRequest -RelativeUri 'identity/conditionalAccess/policies' -ApiVersion beta
-    $asp = Invoke-ZtGraphRequest -RelativeUri 'policies/authenticationStrengthPolicies' -ApiVersion beta
+    $totalUserCount = $results.Length
+    $phishResistantPrivUsers = $results | Where-Object { $_.phishResistantAuthMethod }
+    $phishablePrivUsers = $results | Where-Object { !$_.phishResistantAuthMethod }
 
-    # Get all privileged roles
-    $privilegedRoles = $roles | Where-Object { $_.isPrivileged }
+    $phishablePrivUserCount = $phishablePrivUsers.Length
+    $phishResistantPrivUserCount = $phishResistantPrivUsers.Length
 
-    $phishResAuthMs = @('windowsHelloForBusiness', 'fido2', 'x509CertificateMultiFactor') # Phishing resistant authentication methods (passkey included in fido2)
+    $passed = $totalUserCount -eq $phishResistantPrivUserCount
 
-    # Get all the authentication strength policies that only allow phishing resistant authentication methods
-    $phishResAsp = $asp | Where-Object { (($_.allowedCombinations + $phishResAuthMs | Select-Object -Unique) | Measure-Object).Count -eq ($phishResAuthMs | Measure-Object).Count }
-
-    # Get the IDs of CA policies using phishing resistant auth strength policies
-    $capsIdsUsingPhishResAuth = $phishResAsp | ForEach-Object { Invoke-ZtGraphRequest -RelativeUri "policies/authenticationStrengthPolicies/$($_.id)/usage" }
-
-    # Get the full CA policies that use phishing resistant authentication
-    $capsUsingPhishResAuth = $caps | Where-Object { $_.id -in $capsIdsUsingPhishResAuth.none.id }
-
-    # Are there any privileged roles that are not enforced to use phishing resistant authentication?
-
-    $capCoveredRoles = $capsUsingPhishResAuth.conditions.users.includeroles | Select-Object -Unique
-
-    $protectedRoles = @()
-    $unprotectedRoles = @()
-    foreach ($role in $privilegedRoles) {
-        if ($role.id -in $capCoveredRoles) {
-            $protectedRoles += $role
-        }
-        else {
-            $unprotectedRoles += $role
-        }
-    }
-    $passed = ($unprotectedRoles | Measure-Object).Count -eq 0
     if ($passed) {
-        $testResultMarkdown += "Validated that following accounts use phishing resistant credentials.`n`n%TestResult%"
+        $testResultMarkdown += "Validated that all privileged users have registered phishing resistant authentication methods.`n`n%TestResult%"
     }
     else {
-        $testResultMarkdown += "Found Accounts are using phishable credentials`n`n%TestResult%"
+        $testResultMarkdown += "Found privileged users that have not yet registered phishing resistant authentication methods`n`n%TestResult%"
     }
 
-    $mdInfo += "`n`n## Conditional Access Policies with phishing resistant authentication policies `n`n"
+    $mdInfo = "## Privileged users`n`n"
 
-    if (($capsUsingPhishResAuth | Measure-Object).Count -eq 0) {
-        $mdInfo += "No conditional access policies found with phishing resistant authentication strength policies.`n`n"
+    if ($passed) {
+        $mdInfo += "All privileged registered phishing resistant authentication methods.`n`n"
     }
-    else {
-        $mdInfo += "Found $($capsUsingPhishResAuth.Length) phishing resistant conditional access policies.`n`n"
-        $mdInfo += Get-GraphObjectMarkdown -GraphObjects $capsUsingPhishResAuth -GraphObjectType ConditionalAccess
-    }
-
-    $mdInfo += "`n`n## Privileged Roles`n`n"
-    if (($protectedRoles | Measure-Object).Coiunt -eq 0) {
-        $mdInfo += "Privileged roles are not being protected by phishing resistant authentication.`n`n"
-    }
-    elseif ($protectedRoles.Length -eq $privilegedRoles.Length) {
-        $mdInfo += "All $($protectedRoles.Length) privileged roles are protected by phishing resistant authentication.`n`n"
-    }
-    else {
-        $mdInfo += "Found $($protectedRoles.Length) of $($privilegedRoles.Length) privileged roles protected by phishing resistant authentication.`n`n"
-    }
-    $mdInfo += "| Role Name | Phishing resistance enforced |`n"
-    $mdInfo += "| :--- | :---: |`n"
-    foreach ($role in $protectedRoles | Sort-Object displayName) {
-        $mdInfo += "| $($role.displayName) | ✅ |`n"
+    else{
+        $mdInfo += "Found $phishablePrivUserCount privileged users that have not registered phishing resistant authentication methods.`n`n"
     }
 
-    foreach ($role in $unprotectedRoles | Sort-Object displayName) {
-        $mdInfo += "| $($role.displayName) | ❌ |`n"
+    $mdInfo += "User | Role Name | Phishing resistant method registered |`n"
+    $mdInfo += "| :--- | :--- | :---: |`n"
+
+    $userLinkFormat = "https://entra.microsoft.com/#view/Microsoft_AAD_UsersAndTenants/UserProfileMenuBlade/~/UserAuthMethods/userId/{0}/hidePreviewBanner~/true"
+
+
+    foreach ($user in $phishablePrivUsers | Sort-Object displayName) {
+        $userLink = $userLinkFormat -f $user.id
+        $mdInfo += "|[$($user.userDisplayName)]($userLink)| $($user.roleDisplayName) | ❌ |`n"
     }
 
-
-    if (($phishResAsp | Measure-Object).Count -ne 0) {
-        $mdInfo += "## Authentication Strength Policies`n`n"
-        $mdInfo += "Found $($phishResAsp.Length) custom phishing resistant authentication strength policies.`n`n"
-        $mdInfo += Get-GraphObjectMarkdown -GraphObjects $phishResAsp -GraphObjectType AuthenticationStrength
+    foreach ($user in $phishResistantPrivUsers | Sort-Object displayName) {
+        $userLink = $userLinkFormat -f $user.id
+        $mdInfo += "|[$($user.userDisplayName)]($userLink)| $($user.roleDisplayName) | ✅ |`n"
     }
 
     $testResultMarkdown = $testResultMarkdown -replace "%TestResult%", $mdInfo
 
-    Add-ZtTestResultDetail -TestId '21781' -Title 'Privileged users sign in with phishing resistant methods' `
+    Add-ZtTestResultDetail -TestId '21781' -Title 'Privileged accounts have phishing-resistant methods registered' `
         -UserImpact Low -Risk High -ImplementationCost Medium `
-        -AppliesTo Identity -Tag Credential, TenantPolicy `
+        -AppliesTo Identity -Tag Credential `
         -Status $passed -Result $testResultMarkdown
 }
