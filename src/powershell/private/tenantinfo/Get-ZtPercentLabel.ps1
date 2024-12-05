d302518ec813f54b544fb6a93f728f3e78a4f10f
Merill Fernando <merill@merill.net>
2024-12-05 17:34:30 +1100

    Update Get-ZtPercentLabel.ps1

--- src/powershell/private/tenantinfo/Get-ZtPercentLabel.ps1
+++ src/powershell/private/tenantinfo/Get-ZtPercentLabel.ps1
@@ -5,7 +5,7 @@
     $percent = ($value / $total) * 100
 
     if ($percent -lt 0 -or $percent -gt 100) {
-        throw "Percent must be between 0 and 100"
+        return "0%"
     }
 
     if ($percent -gt 0 -and $percent -lt 1) {
