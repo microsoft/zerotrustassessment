fc1532d7d4ec15b2bcab740fdad80291e57b2c94
annquinzon <akcquinzon@outlook.com>
2025-03-03 10:07:28 -0800

    Adding 21866 - All Microsoft Entra recommendations are addressed

--- src/powershell/private/tests/Test-Assessment.21866.md
+++ src/powershell/private/tests/Test-Assessment.21866.md
@@ -1,6 +1,9 @@
-This test is under construction.
+Microsoft Entra recommendations provide opportunities to implement best practices and optimize your organization’s security posture. Not acting might result in increased surface area of risk or suboptimal operations or user experience.
 
 **Remediation action**
 
+Address each of the high impact recommendations. Each recommendation will have its own set of remediation steps: 
+
+- [What are Microsoft Entra recommendations?](https://learn.microsoft.com/en-us/entra/identity/monitoring-health/overview-recommendations#recommendations-overview-table)
 <!--- Results --->
 %TestResult%
--- src/powershell/private/tests/Test-Assessment.21866.ps1
+++ src/powershell/private/tests/Test-Assessment.21866.ps1
@@ -10,15 +10,33 @@ function Test-Assessment-21866{
     Write-PSFMessage '🟦 Start' -Tag Test -Level VeryVerbose
 
     $activity = "Checking All Microsoft Entra recommendations are addressed"
-    Write-ZtProgress -Activity $activity -Status "Getting policy"
+    Write-ZtProgress -Activity $activity
 
-    $result = $false
-    $testResultMarkdown = "Under construction."
-    $passed = $result
+    $recommendations = Invoke-ZtGraphRequest -RelativeUri "directory/recommendations" -ApiVersion beta
+    $result = $recommendations | Where-Object { $_.status -in @('active', 'postponed') }
 
+    Write-Output $result.Count
+    $passed = $result.Count -eq 0
+    if ($passed) {
+        $testResultMarkdown = "All Entra Recommendations are addressed.`n`n"
+    }
+    else {
+        $testResultMarkdown = "Found $($result.Count) unaddressed Entra recommendations.`n`n%TestResult%"
+    }
+
+    if ($result.Count -gt 0) {
+        $mdInfo = "`n## Unaddressed Entra recommendations`n`n"
+        $mdInfo += "| Display Name | Status | Insights | Priority |`n"
+        $mdInfo += "| :--- | :--- | :--- | :--- |`n"
+        foreach ($item in $result) {
+            $mdInfo += "| $($item.displayName) | $($item.status) | $($item.Insights) | $($item.priority) |`n"
+        }
+    }
+
+    $testResultMarkdown = $testResultMarkdown -replace "%TestResult%", $mdInfo
 
     Add-ZtTestResultDetail -TestId '21866' -Title "All Microsoft Entra recommendations are addressed" `
         -UserImpact Low -Risk Medium -ImplementationCost High `
         -AppliesTo Identity -Tag Identity `
-        -Status $passed -Result $testResultMarkdown -SkippedBecause UnderConstruction
+        -Status $passed -Result $testResultMarkdown
 }
