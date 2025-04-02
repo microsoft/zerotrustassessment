dcf58bf3f90969bdf964878178035b93c3138c62
annquinzon <akcquinzon@outlook.com>
2025-04-01 17:33:16 -0700

    Assessment 21992: Application Certificates need to be rotated on a regular basis

--- src/powershell/private/tests/Test-Assessment.21992.md
+++ src/powershell/private/tests/Test-Assessment.21992.md
@@ -1,6 +1,8 @@
-...
+Certificates, if not rotated regularly, can give an extended window for threat actors to attempt to extract and exploit those certificates, leading to unauthorized access. Credentials, when exposed, provide attackers with legitimate credentials to blend their activities with legitimate operations, making it easier to bypass security controls. If an attacker compromises an application’s certificate, they can escalate their privileges within the system, leading to broader access and control, depending on the privileges of the application. 
 
 **Remediation action**
+- [Define certificate based application configuration](https://learn.microsoft.com/en-us/graph/api/resources/certificatebasedapplicationconfiguration?view=graph-rest-beta)
+- [Create a least-privileged custom role to rotate application credentials](https://learn.microsoft.com/en-us/entra/identity/role-based-access-control/custom-create)
 
 <!--- Results --->
 %TestResult%
--- src/powershell/private/tests/Test-Assessment.21992.ps1
+++ src/powershell/private/tests/Test-Assessment.21992.ps1
@@ -12,13 +12,96 @@ function Test-Assessment-21992{
     $activity = "Checking Application Certificates need to be rotated on a regular basis"
     Write-ZtProgress -Activity $activity -Status "Getting policy"
 
-    $result = $false
-    $testResultMarkdown = "Planned for future release."
-    $passed = $result
+    # Calculate date 180 days ago
+    $thresholdDate = (Get-Date).AddDays(-180)
 
+    # Initialize the sp result variable
+    $spresult = @()
+
+    # Query enabled service principals
+    $serviceprincipals = Invoke-ZtGraphRequest -RelativeUri "servicePrincipals" -ApiVersion beta
+    $enabledServicePrincipals = $serviceprincipals | Where-Object { $_.accountEnabled -eq $true }
+
+    foreach($sp in $enabledServicePrincipals) {
+        if ($sp.keyCredentials) {
+            foreach ($keyCredential in $sp.keyCredentials) {
+                $startDate = [datetime]::Parse($keyCredential.startDateTime)
+                if ($startDate -lt $thresholdDate) {
+                    $spresult += $sp
+                    break
+                }
+            }
+        }
+    }
+
+    # Initialize the app result variable
+    $appresult = @()
+
+     # Query all app registrations
+     $appregistrations = Invoke-ZtGraphRequest -RelativeUri "applications" -ApiVersion beta
+
+     foreach($app in $appregistrations) {
+        if ($app.keyCredentials) {
+            foreach ($keyCredential in $app.keyCredentials) {
+                $startDate = [datetime]::Parse($keyCredential.startDateTime)
+                if ($startDate -lt $thresholdDate) {
+                    $appresult += $app
+                    break
+                }
+            }
+        }
+    }
+
+    $totalcount = $spresult.Count + $appresult.Count
+    $passed = $totalcount -eq 0
+
+    if ($passed) {
+        $testResultMarkdown = "Applications in your tenant that have certificates have been issued within 180 days.`n`n"
+    }
+    else {
+        $testResultMarkdown = "Found $($totalcount) applications in your tenant with certificates that have not been rotated within 180 days.`n`n%TestResult%"
+    }
+
+
+    if($totalcount -gt 0) {
+        $mdInfo = "`n## Applications with certificates that have not been rotated within 180 days`n`n"
+        $mdInfo += "| App ID | Display Name | Certificate Thumbprint | Certificate Start Date | Certificate Expiration Date | Multi-tenant | Owner Tenant ID |`n"
+        $mdInfo += "| :--- | :--- | :--- | :--- | :--- | :--- | :--- |`n"
+
+        if($spresult.Count -gt 0) {
+            foreach ($sp in $spresult) {
+                if ($sp.signInAudience -eq "AzureADMultipleOrgs") {
+                    $ismultitenant = "Yes"
+                } else {
+                    $ismultitenant = "No"
+                }
+
+                foreach ($keyCredential in $sp.keyCredentials) {
+                    $mdInfo += "| $($sp.appId) | $($sp.displayName) | $($keyCredential.customKeyIdentifier) | $($keyCredential.startDateTime) | $($keyCredential.endDateTime) | $($ismultitenant) | $($sp.appOwnerOrganizationId) |`n"
+                }
+            }
+        }
+
+        if($appresult.Count -gt 0) {
+            foreach ($app in $appresult) {
+                if ($app.signInAudience -eq "AzureADMultipleOrgs") {
+                    $ismultitenant = "Yes"
+                } else {
+                    $ismultitenant = "No"
+                }
+
+                foreach ($keyCredential in $app.keyCredentials) {
+                    $mdInfo += "| $($app.appId) | $($app.displayName) | $($keyCredential.customKeyIdentifier) | $($keyCredential.startDateTime) | $($keyCredential.endDateTime) | $($ismultitenant) | $($app.publisherDomain) |`n"
+                }
+            }
+        }
+
+    }
+
+    $testResultMarkdown = $testResultMarkdown -replace "%TestResult%", $mdInfo
 
     Add-ZtTestResultDetail -TestId '21992' -Title "Application Certificates need to be rotated on a regular basis" `
         -UserImpact Low -Risk High -ImplementationCost High `
         -AppliesTo Identity -Tag Identity `
-        -Status $passed -Result $testResultMarkdown -SkippedBecause UnderConstruction
+        -Status $passed -Result $testResultMarkdown
 }
