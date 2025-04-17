755fe19f2f6bb101b60feae0aa5359ddf26740d0
annquinzon <akcquinzon@outlook.com>
2025-04-16 19:15:28 -0700

    Added 21787
    Added test 21787 - Permissions to create new tenants is limited to the Tenant Creator role

--- src/powershell/private/tests/Test-Assessment.21787.ps1
+++ src/powershell/private/tests/Test-Assessment.21787.ps1
@@ -12,13 +12,18 @@ function Test-Assessment-21787{
     $activity = "Checking Permissions to create new tenants is limited to the Tenant Creator role"
     Write-ZtProgress -Activity $activity -Status "Getting policy"
 
-    $result = $false
-    $testResultMarkdown = "Planned for future release."
-    $passed = $result
+    $result = Invoke-ZtGraphRequest -RelativeUri "policies/authorizationPolicy" -ApiVersion v1.0
+    $passed = -not $result.defaultUserRolePermissions.allowedToCreateTenants
 
+    if ($passed) {
+        $testResultMarkdown = "Non-privileged users are restricted from creating tenants.`n`n"
+    }
+    else {
+        $testResultMarkdown = "Non-privileged users are allowed to create tenants. The defaultUserPermissions.allowedToCreateTenants property is set to true.`n`n"
+    }
 
     Add-ZtTestResultDetail -TestId '21787' -Title "Permissions to create new tenants is limited to the Tenant Creator role" `
         -UserImpact Low -Risk High -ImplementationCost Low `
         -AppliesTo Identity -Tag Identity `
-        -Status $passed -Result $testResultMarkdown -SkippedBecause UnderConstruction
+        -Status $passed -Result $testResultMarkdown
 }
