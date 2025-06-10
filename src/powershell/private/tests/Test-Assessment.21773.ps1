149b53ddf8c77d107c18b4ef2f6f8b5d1393a6a0
Aleksandar Nikolić <alexandair@live.com>
2025-06-10 08:42:04 +0000

    Remove array wrapping
    Remove array wrapping

--- src/powershell/private/tests/Test-Assessment.21773.ps1
+++ src/powershell/private/tests/Test-Assessment.21773.ps1
@@ -31,8 +31,8 @@ function Test-Assessment-21773 {
     order by displayName, keyEndDateTime DESC
 "@
 
-    $resultsApp = @(Invoke-DatabaseQuery -Database $Database -Sql $sqlApp)
-    $resultsSP = @(Invoke-DatabaseQuery -Database $Database -Sql $sqlSP)
+    $resultsApp = Invoke-DatabaseQuery -Database $Database -Sql $sqlApp
+    $resultsSP = Invoke-DatabaseQuery -Database $Database -Sql $sqlSP
 
     $passed = ($resultsApp.Count -eq 0) -and ($resultsSP.Count -eq 0)
 
