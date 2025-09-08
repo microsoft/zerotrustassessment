a72b48f6a30af3407aae2c976b1c37ad9f47ad2e
alexandair <alexandair@live.com>
2025-09-08 09:13:59 +0000

    Implement Get-PolicyAssignmentTarget function and remove duplicates from assessment tests

--- src/powershell/tests/Test-Assessment.24540.ps1
+++ src/powershell/tests/Test-Assessment.24540.ps1
@@ -10,58 +10,6 @@ function Test-Assessment-24540 {
     [CmdletBinding()]
     param()
 
-    #region Helper Functions
-    function Get-PolicyAssignmentTarget {
-        [CmdletBinding()]
-        param(
-            [Parameter(Mandatory = $true)]
-            $Assignments
-        )
-
-        if (-not $Assignments -or $Assignments.Count -eq 0) {
-            return "None"
-        }
-
-        $includedTargets = @()
-        $excludedTargets = @()
-
-        foreach ($assignment in $Assignments) {
-            $isExcluded = $assignment.target.'@odata.type' -eq '#microsoft.graph.exclusionGroupAssignmentTarget'
-
-            if ($assignment.target.groupId -and $assignment.target.groupId -ne "") {
-                $groupName = Invoke-ZtGraphRequest -RelativeUri "groups/$($assignment.target.groupId)?`$select=displayName" -ApiVersion v1.0
-                if ($isExcluded) {
-                    $excludedTargets += $groupName.displayName
-                }
-                else {
-                    $includedTargets += $groupName.displayName
-                }
-            }
-            elseif ($assignment.target.'@odata.type' -eq '#microsoft.graph.allDevicesAssignmentTarget') {
-                $includedTargets += "All Devices"
-            }
-            elseif ($assignment.target.'@odata.type' -eq '#microsoft.graph.allLicensedUsersAssignmentTarget') {
-                $includedTargets += "All Users"
-            }
-        }
-
-        # Build grouped assignment target string
-        $assignmentParts = @()
-        if ($includedTargets.Count -gt 0) {
-            $assignmentParts += "**Included:** " + ($includedTargets -join ", ")
-        }
-        if ($excludedTargets.Count -gt 0) {
-            $assignmentParts += "**Excluded:** " + ($excludedTargets -join ", ")
-        }
-
-        if ($assignmentParts.Count -gt 0) {
-            return $assignmentParts -join ", "
-        }
-
-        return "None"
-    }
-    #endregion Helper Functions
-
     #region Data Collection
     Write-PSFMessage '🟦 Start' -Tag Test -Level VeryVerbose
 
