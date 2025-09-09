cb8d7c206901d646bb157252433d109fb1c3bfde
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2025-09-09 09:42:59 +0200

    adding tests

---
+++ build/powershell/Install-Prerequisites.ps1
@@ -0,0 +1,28 @@
+﻿[CmdletBinding()]
+param (
+	[string]
+	$Repository = 'PSGallery'
+)
+
+Invoke-WebRequest 'https://raw.githubusercontent.com/PowershellFrameworkCollective/PSFramework.NuGet/refs/heads/master/bootstrap.ps1' | Invoke-Expression
+
+$modules = @("Pester", "PSScriptAnalyzer")
+
+# Automatically add missing dependencies
+$data = Import-PowerShellDataFile -Path "$PSScriptRoot\..\..\src\powershell\ZeroTrustAssessmentV2.psd1"
+foreach ($dependency in $data.RequiredModules) {
+	if ($dependency -is [string]) {
+		if ($modules -contains $dependency) {
+			continue
+		}
+		$modules += $dependency
+	}
+	else {
+		if ($modules -contains $dependency.ModuleName) {
+			continue
+		}
+		$modules += $dependency.ModuleName
+	}
+}
+
+Install-PSFModule -Name $modules
---
+++ code-tests/general/OneCommandPerFile.Tests.ps1
@@ -0,0 +1,9 @@
+﻿Describe "Each function file should only contain one command" {
+	$fileGroups = Get-ChildItem -Path "$($global:__testData.ModuleRoot)\public", "$($global:__testData.ModuleRoot)\private" -Recurse | Where-Object Name -Like "*.ps1" | Get-AstFunctionDefinition | Group-Object FilePath
+
+	foreach ($fileGroup in $fileGroups) {
+		It "Should have only one command in $($fileGroup.Name)" -TestCases @{ fileGroup = $fileGroup} {
+			$fileGroup.Group.Name | Should -HaveCount 1
+		}
+	}
+}
