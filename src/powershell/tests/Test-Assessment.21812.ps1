ab16eafd7a8a3723a64f47a5d88a5fa978514a3f
Aleksandar Nikolić <alexandair@live.com>
2025-09-16 00:30:50 +0000

    21812 - Number of Global Administrators (#447)

--- src/powershell/tests/Test-Assessment.21812.ps1
+++ src/powershell/tests/Test-Assessment.21812.ps1
@@ -3,22 +3,99 @@
 
 #>
 
-function Test-Assessment-21812{
+function Test-Assessment-21812 {
     [CmdletBinding()]
     param()
 
+    #region Data Collection
     Write-PSFMessage '🟦 Start' -Tag Test -Level VeryVerbose
 
     $activity = "Checking Maximum number of Global Administrators doesn't exceed eight users"
     Write-ZtProgress -Activity $activity -Status "Getting policy"
 
-    $result = $false
-    $testResultMarkdown = "Planned for future release."
-    $passed = $result
+    # Retrieve all Global Administrators
+    $allGlobalAdmins = Get-ZtRoleMember -Role GlobalAdministrator
+    # Exclude groups from the count
+    $globalAdmins = @($allGlobalAdmins | Where-Object { $_.'@odata.type' -in @('#microsoft.graph.user', '#microsoft.graph.servicePrincipal') })
 
+    #endregion Data Collection
 
-    Add-ZtTestResultDetail -TestId '21812' -Title "Maximum number of Global Administrators doesn't exceed eight users" `
-        -UserImpact Low -Risk Low -ImplementationCost Low `
-        -AppliesTo Identity -Tag Identity `
-        -Status $passed -Result $testResultMarkdown -SkippedBecause UnderConstruction
+    #region Assessment Logic
+    $passed = $false
+    $testResultMarkdown = ""
+
+if ($globalAdmins.Count -gt 8) {
+        $passed = $false
+    }
+    else {
+        $passed = $true
+    }
+
+    if ($passed) {
+        $testResultMarkdown = "Maximum number of Global Administrators doesn't exceed eight users/service principals.`n`n%TestResult%"
+    }
+    else {
+        $testResultMarkdown = "Maximum number of Global Administrators exceeds eight users/service principals.`n`n%TestResult%"
+    }
+    #endregion Assessment Logic
+
+    #region Report Generation
+    # Build the detailed sections of the markdown
+
+    # Define variables to insert into the format string
+    $reportTitle = "Global Administrators"
+    $tableRows = ""
+
+    if ($globalAdmins.Count -gt 0) {
+        # Create a here-string with format placeholders {0}, {1}, etc.
+        $formatTemplate = @'
+
+## {0}
+
+### Total number of Global Administrators: {1}
+
+| Display Name | Object Type | User Principal Name |
+| :----------- | :---------- | :------------------ |
+{2}
+
+'@
+
+        foreach ($globalAdmin in $globalAdmins) {
+            $displayName = $globalAdmin.DisplayName
+
+            $objectType = switch ($globalAdmin.'@odata.type') {
+                '#microsoft.graph.user' { 'User' }
+                '#microsoft.graph.servicePrincipal' { 'Service Principal' }
+                default { 'Unknown' }
+            }
+
+            $userPrincipalName = if ($globalAdmin.UserPrincipalName) { $globalAdmin.UserPrincipalName } else { 'N/A' }
+
+            # Create portal link based on object type
+            $portalLink = switch ($objectType) {
+                'User' { "https://entra.microsoft.com/#view/Microsoft_AAD_UsersAndTenants/UserProfileMenuBlade/~/AdministrativeRole/userId/$($globalAdmin.Id)" }
+                'Service Principal' { "https://entra.microsoft.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Overview/appId/$($globalAdmin.AppId)" }
+                default { "https://entra.microsoft.com" }
+            }
+
+            $tableRows += @"
+| [$(Get-SafeMarkdown($displayName))]($portalLink) | $objectType | $userPrincipalName |`n
+"@
+        }
+
+        # Format the template by replacing placeholders with values
+        $mdInfo = $formatTemplate -f $reportTitle, $globalAdmins.Count, $tableRows
+    }
+
+    # Replace the placeholder with the detailed information
+    $testResultMarkdown = $testResultMarkdown -replace "%TestResult%", $mdInfo
+    #endregion Report Generation
+
+    $params = @{
+        TestId = '21812'
+        Title  = "Maximum number of Global Administrators doesn't exceed eight users"
+        Status = $passed
+        Result = $testResultMarkdown
+    }
+    Add-ZtTestResultDetail @params
 }
