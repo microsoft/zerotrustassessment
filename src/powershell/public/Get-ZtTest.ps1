632d8c5a03742034d898d27b47f2c5828b7d657d
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2025-09-18 11:18:03 +0200

    Updated test handling (start)

---
+++ src/powershell/public/Get-ZtTest.ps1
@@ -0,0 +1,24 @@
+﻿function Get-ZtTest {
+	[CmdletBinding()]
+	param (
+		[string[]]
+		$ID,
+
+		[string[]]
+		$Pillar,
+
+		[ValidateSet('Workforce', 'External')]
+		[string]
+		$TenantType
+	)
+	begin {
+		if (-not $script:__ZtSession.TestMeta) {
+			$script:__ZtSession.TestMeta = foreach ($command in Get-Command Test-Assessment-* -Module ZeroTrustAssessmentV2) {
+				Get-ZtTestMetadata -Command $command
+			}
+		}
+	}
+	process {
+
+	}
+}
