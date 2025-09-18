9eea8caa1a636d92d30dce969aaea2f83c376209
merill <merill@users.noreply.github.com>
2025-09-18 23:28:55 +1000

    Update Add-ZTDeviceCompliancePolicies.ps1

--- src/powershell/private/tenantinfo/devices/Add-ZTDeviceCompliancePolicies.ps1
+++ src/powershell/private/tenantinfo/devices/Add-ZTDeviceCompliancePolicies.ps1
@@ -8,69 +8,125 @@ function Add-ZTDeviceCompliancePolicies {
 
     function Get-DefenderEndPointLabel($advancedThreatProtectionRequiredSecurityLevel) {
         switch ($advancedThreatProtectionRequiredSecurityLevel) {
-            'high' { return 'High' }
-            'medium' { return 'Medium' }
-            'low' { return 'Low' }
-            'secured' { return 'Clear' }
-            'unavailable' { return '' }
-            'notSet' { return '' }
-            default { return '' }
+            'high' {
+                return 'High'
+            }
+            'medium' {
+                return 'Medium'
+            }
+            'low' {
+                return 'Low'
+            }
+            'secured' {
+                return 'Clear'
+            }
+            'unavailable' {
+                return ''
+            }
+            'notSet' {
+                return ''
+            }
+            default {
+                return ''
+            }
         }
     }
 
     function Get-DeviceThreatLevelLabel($deviceThreatProtectionRequiredSecurityLevel) {
         switch ($deviceThreatProtectionRequiredSecurityLevel) {
-            'high' { return 'High' }
-            'medium' { return 'Medium' }
-            'low' { return 'Low' }
-            'secured' { return 'Secured' }
-            'unavailable' { return '' }
-            'notSet' { return '' }
-            default { return '' }
+            'high' {
+                return 'High'
+            }
+            'medium' {
+                return 'Medium'
+            }
+            'low' {
+                return 'Low'
+            }
+            'secured' {
+                return 'Secured'
+            }
+            'unavailable' {
+                return ''
+            }
+            'notSet' {
+                return ''
+            }
+            default {
+                return ''
+            }
         }
     }
 
     function Get-PasswordRequiredType($passwordType) {
 
         switch ($passwordType) {
-            'deviceDefault' { return 'Device default' }
-            'alphanumeric' { return 'Alphanumeric' }
-            'numeric' { return 'Numeric' }
-            'alphabetic' { return 'Alphabetic'}
-            'alphanumericWithSymbols' { return 'Alphanumeric with symbols'}
-            'lowSecurityBiometric' { return 'Biometric (low security)'}
-            'customPassword' { return 'Custom password'}
-            'required' { return 'Password required'}
-            'Any' { return 'Any'}
-            default { return $passwordType }
+            'deviceDefault' {
+                return 'Device default'
+            }
+            'alphanumeric' {
+                return 'Alphanumeric'
+            }
+            'numeric' {
+                return 'Numeric'
+            }
+            'alphabetic' {
+                return 'Alphabetic'
+            }
+            'alphanumericWithSymbols' {
+                return 'Alphanumeric with symbols'
+            }
+            'lowSecurityBiometric' {
+                return 'Biometric (low security)'
+            }
+            'customPassword' {
+                return 'Custom password'
+            }
+            'required' {
+                return 'Password required'
+            }
+            'Any' {
+                return 'Any'
+            }
+            default {
+                return $passwordType
+            }
         }
     }
 
-    function Get-AndroidCompliancePolicy($Policy) {
+    function Get-ActionForNoncomplianceDays ($scheduledActionsForRule, $actionType) {
+        $action = $scheduledActionsForRule | Where-Object { $_.actionType -eq $actionType }
+        if ($action) {
+            return $action.daysAfterComplianceGracePeriodEnd
+        } else {
+            return ''
+        }
+    }
+    function Get-CompliancePolicyView($Policy, $Platform) {
         return [PSCustomObject]@{
-            Platform = 'Android device administrator'
-            PolicyName = $Policy.displayName
-            DefenderForEndPoint = Get-DefenderEndPointLabel $Policy.AdvancedThreatProtectionRequiredSecurityLevel
-            MinOsVersion = $Policy.OsMinimumVersion
-            MaxOsVersion = $Policy.OsMaximumVersion
-            RequirePswd = $Policy.passwordRequired -eq 'true' ? 'Yes' : ''
-            MinPswdLength = $Policy.PasswordMinimumLength
-            PasswordType = Get-PasswordRequiredType $Policy.passwordType
-            PswdExpiryDays = $Policy.PasswordExpirationDays
-            CountOfPreviousPswdToBlock = $Policy.PasswordPreviousPasswordBlockCount
-            #MaxInactivityMin = $Policy.maxInactivityTimeDeviceLockInMinutes
-            RequireEncryption = $Policy.storageRequireEncryption -eq 'true' ? 'Yes' : ''
-            RootedJailbrokenDevices = $Policy.securityBlockJailbrokenDevices -eq 'true' ? 'Blocked' : ''
-            MaxDeviceThreatLevel = Get-DeviceThreatLevelLabel $Policy.deviceThreatProtectionLevel
-            RequireFirewall = ''
-            ActionForNoncomplianceDaysPushNotification = Get-ActionForNoncomplianceDays($Policy.scheduledActionsForRule, 'pushNotification')
-            ActionForNoncomplianceDaysSendEmail = Get-ActionForNoncomplianceDays($Policy.scheduledActionsForRule, 'sendEmail')
-            ActionForNoncomplianceDaysRemoteLock = Get-ActionForNoncomplianceDays($Policy.scheduledActionsForRule, 'remoteLock')
-            ActionForNoncomplianceDaysBlock = Get-ActionForNoncomplianceDays($Policy.scheduledActionsForRule, 'block')
-            ActionForNoncomplianceDaysRetire = Get-ActionForNoncomplianceDays($Policy.scheduledActionsForRule, 'retire')
-            Scope = ''
-            IncludedGroups = ($Policy.assignments | Where-Object { $_.target.groupId -and $_.target.includeAllDevices -eq $false } | ForEach-Object { Get-GroupName $_.target.groupId }) -join ", "
-            ExcludedGroups = ($Policy.assignments | Where-Object { $_.target.groupId -and $_.target.excludeAllDevices -eq $true } | ForEach-Object { Get-GroupName $_.target.groupId }) -join ", "
+            Platform                                   = $Platform
+            PolicyName                                 = $Policy.displayName
+            DefenderForEndPoint                        = Get-DefenderEndPointLabel $Policy.AdvancedThreatProtectionRequiredSecurityLevel
+            MinOsVersion                               = $Policy.OsMinimumVersion
+            MaxOsVersion                               = $Policy.OsMaximumVersion
+            RequirePswd                                = $Policy.passwordRequired -eq 'true' ? 'Yes' : ''
+            MinPswdLength                              = $Policy.PasswordMinimumLength
+            PasswordType                               = Get-PasswordRequiredType $Policy.passwordType
+            PswdExpiryDays                             = $Policy.PasswordExpirationDays
+            CountOfPreviousPswdToBlock                 = $Policy.PasswordPreviousPasswordBlockCount
+            RequireEncryption                          = $Policy.storageRequireEncryption -eq 'true' ? 'Yes' : ''
+            RootedJailbrokenDevices                    = $Policy.securityBlockJailbrokenDevices -eq 'true' ? 'Blocked' : ''
+            MaxDeviceThreatLevel                       = Get-DeviceThreatLevelLabel $Policy.DeviceThreatProtectionRequiredSecurityLevel
+            RequireFirewall                            = ''
+            MaxInactivityMin                           = $Policy.PasswordMinutesOfInactivityBeforeLock
+            ActionForNoncomplianceDaysPushNotification = ''
+            ActionForNoncomplianceDaysSendEmail        = ''
+            ActionForNoncomplianceDaysRemoteLock       = ''
+            ActionForNoncomplianceDaysBlock            = ''
+            ActionForNoncomplianceDaysRetire           = ''
+            Scope                                      = ''
+            IncludedGroups                             = ($Policy.assignments | Where-Object { $_.target.groupId -and $_.target.includeAllDevices -eq $false } | ForEach-Object { Get-GroupName $_.target.groupId }) -join ", "
+            ExcludedGroups                             = ($Policy.assignments | Where-Object { $_.target.groupId -and $_.target.excludeAllDevices -eq $true } | ForEach-Object { Get-GroupName $_.target.groupId }) -join ", "
         }
     }
 
@@ -79,69 +135,78 @@ function Add-ZTDeviceCompliancePolicies {
 
     $compliancePolicies = Invoke-ZtGraphRequest -RelativeUri 'deviceManagement/deviceCompliancePolicies' -QueryParameters @{ '$expand' = 'assignments,scheduledActionsForRule($expand=scheduledActionConfigurations)' } -ApiVersion 'beta'
 
-    $linuxCompliancePolicies = Invoke-ZtGraphRequest -RelativeUri 'deviceManagement/deviceCompliancePolicies' -QueryParameters @{ '$expand' = 'assignments,scheduledActionsForRule($expand=scheduledActionConfigurations)' } -ApiVersion 'beta'
+    #$linuxCompliancePolicies = Invoke-ZtGraphRequest -RelativeUri 'deviceManagement/deviceCompliancePolicies' -QueryParameters @{ '$expand' = 'assignments,scheduledActionsForRule($expand=scheduledActionConfigurations)' } -ApiVersion 'beta'
 
     # Create the table data structure
     $tableData = @()
     foreach ($policy in $compliancePolicies) {
 
-        # switch $policy.'@odata.type'{
-        #     '#microsoft.graph.androidCompliancePolicy' {
-        #         $tableData += Get-AndroidCompliancePolicy -Policy $policy
-        #     }
+        switch ($policy.'@odata.type') {
+            '#microsoft.graph.androidCompliancePolicy' {
+                $policyView = Get-CompliancePolicyView -Policy $policy -Platform 'Android device administrator'
+                $policyView.RequireFirewall = 'Not Applicable'
+            }
+            '#microsoft.graph.androidDeviceOwnerCompliancePolicy' {
+                $policyView = Get-CompliancePolicyView -Policy $policy -Platform 'Android Enterprise (Corp)'
+                $policyView.RequireFirewall = 'Not Applicable'
+            }
+            '#microsoft.graph.androidWorkProfileCompliancePolicy' {
+                $policyView = Get-CompliancePolicyView -Policy $policy -Platform 'Android Enterprise (Personal)'
+                $policyView.RequireFirewall = 'Not Applicable'
 
-        #     '#microsoft.graph.androidDeviceOwnerCompliancePolicy' {
-        #         $tableData += Get-AndroidDeviceOwnerCompliancePolicy -Policy $policy
-        #     }
-        #     '#microsoft.graph.androidWorkProfileCompliancePolicy' {
-        #         $tableData += Get-AndroidWorkProfileCompliancePolicy -Policy $policy
-        #     }
-        #     '#microsoft.graph.aospDeviceOwnerCompliancePolicy' {
-        #         $tableData += Get-AospDeviceOwnerCompliancePolicy -Policy $policy
-        #     }
-        #     '#microsoft.graph.iosCompliancePolicy' {
-        #         $tableData += Get-IosCompliancePolicy -Policy $policy
-        #     }
-        #     '#microsoft.graph.macOSCompliancePolicy' {
-        #         $tableData += Get-MacOSCompliancePolicy -Policy $policy
-        #     }
-        #     '#microsoft.graph.windows10CompliancePolicy' {
-        #         $tableData += Get-Windows10CompliancePolicy -Policy $policy
-        #     }
-        #     '#microsoft.graph.windows81CompliancePolicy' {
-        #         $tableData += Get-Windows81CompliancePolicy -Policy $policy
-        #     }
-        # }
+            }
+            '#microsoft.graph.aospDeviceOwnerCompliancePolicy' {
+                $policyView = Get-CompliancePolicyView -Policy $policy -Platform 'Android (AOSP)'
+                $policyView.DefenderForEndPoint = 'Not Applicable'
+                $policyView.PswdExpiryDays = 'Not Applicable'
+                $policyView.CountOfPreviousPswdToBlock = 'Not Applicable'
+                $policyView.MaxDeviceThreatLevel = 'Not Applicable'
+                $policyView.RequireFirewall = 'Not Applicable'
+            }
+            '#microsoft.graph.iosCompliancePolicy' {
+                $policyView = Get-CompliancePolicyView -Policy $policy -Platform 'iOS/iPadOS'
+                $policyView.RequireFirewall = 'Not Applicable'
+                $policyView.RequirePswd = $policy.PasscodeRequired
+                $policyView.MinPswdLength = $policy.PasscodeMinimumLength
+                $policyView.PswdExpiryDays = $policy.PasscodeExpirationDays
+                $policyView.MaxInactivityMin = $policy.PasscodeMinutesOfInactivityBeforeLock
+                $policyView.CountOfPreviousPswdToBlock = $policy.PasscodePreviousPasscodeBlockCount
+                $policyView.PasswordType = Get-PasswordRequiredType $Policy.PasscodeRequiredType
+                $policyView.RequireEncryption = 'Not Applicable'
+            }
+            '#microsoft.graph.macOSCompliancePolicy' {
+                $policyView = Get-CompliancePolicyView -Policy $policy -Platform 'macOS'
+                $policyView.RootedJailbrokenDevices = 'Not Applicable'
+                $policyView.RequireFirewall = $policy.FirewallEnabled -eq 'true' ? 'Yes' : ''
+            }
+            '#microsoft.graph.windows10CompliancePolicy' {
+                $policyView = Get-CompliancePolicyView -Policy $policy -Platform 'Windows 10 and later'
+                $policyView.DefenderForEndPoint = Get-DefenderEndPointLabel $Policy.DeviceThreatProtectionRequiredSecurityLevel
 
-        # foreach($policy in $linuxCompliancePolicies){
-        #     $tableData += Get-LinuxCompliancePolicy -Policy $policy
-        # }
+                $policyView.RequireFirewall = $policy.ActiveFirewallRequired -eq 'true' ? 'Yes' : ''
+                $policyView.MaxDeviceThreatLevel = 'Not Applicable'
+                $policyView.RootedJailbrokenDevices = 'Not Applicable'
+                $policyView.RequireFirewall = 'Not Applicable'
+            }
+            '#microsoft.graph.windows81CompliancePolicy' {
+                $policyView = Get-CompliancePolicyView -Policy $policy -Platform 'Windows 8.1 and later'
+                $policyView.DefenderForEndPoint = 'Not Applicable'
+                $policyView.MaxDeviceThreatLevel = 'Not Applicable'
+                $policyView.RootedJailbrokenDevices = 'Not Applicable'
+                $policyView.RequireFirewall = 'Not Applicable'
+            }
+            default {
+                $typeName = $policy.'@odata.type' -replace '#microsoft.graph.', ''
+                $policyView = Get-CompliancePolicyView -Policy $policy -Platform $typeName
+            }
 
-        $tableData += [PSCustomObject]@{
-            Platform = ''
-            PolicyName = $policy.displayName
-            DefenderForEndPoint = ''
-            MinOsVersion = ''
-            MaxOsVersion = ''
-            RequirePswd = ''
-            MinPswdLength = ''
-            PasswordType = ''
-            PswdExpiryDays = ''
-            CountOfPreviousPswdToBlock = ''
-            MaxInactivityMin = ''
-            RequireEncryption = ''
-            RootedJailbrokenDevices = ''
-            MaxDeviceThreatLevel = ''
-            RequireFirewall = ''
-            ActionForNoncomplianceDaysPushNotification = ''
-            ActionForNoncomplianceDaysSendEmail = ''
-            ActionForNoncomplianceDaysRemoteLock = ''
-            ActionForNoncomplianceDaysBlock = ''
-            ActionForNoncomplianceDaysRetire = ''
-            Scope = ''
-            IncludedGroups = ''
-            ExcludedGroups = ''
+            # foreach($policy in $linuxCompliancePolicies){
+            #     $tableData += Get-LinuxCompliancePolicy -Policy $policy
+            # }
+
+
         }
+        $tableData += $policyView
     }
 
 
