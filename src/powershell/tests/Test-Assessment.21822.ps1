ec972a152d679b02d8423673a34a5303d5108b95
Komal <komal.p@perennialsys.com>
2025-09-19 11:18:57 +0530

    updating layout

--- src/powershell/tests/Test-Assessment.21822.ps1
+++ src/powershell/tests/Test-Assessment.21822.ps1
@@ -9,8 +9,8 @@ function Test-Assessment-21822 {
 
     Write-PSFMessage '🟦 Start' -Tag Test -Level VeryVerbose
 
-    $activity = "Checking if domain-based allow/deny lists is configured"
-    Write-ZtProgress -Activity $activity
+    $activity = "Checking Guest access is limited to approved tenants"
+    Write-ZtProgress -Activity $activity -Status "Getting policy"
 
     $result = Invoke-ZtGraphRequest -RelativeUri "legacy/policies" -ApiVersion beta
     $b2BManagementPolicyDefinition = ($result | Where-Object -FilterScript {$_.Type -eq "B2BManagementPolicy"}).definition
@@ -25,8 +25,8 @@ function Test-Assessment-21822 {
         $testResultMarkdown = "Guest access is note limited to approved tenants ❌"
     }
 
-    Add-ZtTestResultDetail -TestId '21822' -Title 'Guest access is limited to approved tenants' `
+    Add-ZtTestResultDetail -TestId '21822' -Title "Guest access is limited to approved tenants" `
         -UserImpact Medium -Risk Medium -ImplementationCost High `
-        -AppliesTo Identity -Tag Identity  `
+        -AppliesTo Identity -Tag Identity `
         -Status $passed -Result $testResultMarkdown
 }
