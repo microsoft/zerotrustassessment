82d8177abd69af3b46a6193949d86452d6e12fb0
Komal <komal.p@perennialsys.com>
2025-09-19 10:58:38 +0530

    adding test 21822

--- src/powershell/tests/Test-Assessment.21822.ps1
+++ src/powershell/tests/Test-Assessment.21822.ps1
@@ -1,24 +1,32 @@
 <#
 .SYNOPSIS
-
+    Checks if domain-based allow/deny lists is configured
 #>
 
-function Test-Assessment-21822{
+function Test-Assessment-21822 {
     [CmdletBinding()]
     param()
 
     Write-PSFMessage '🟦 Start' -Tag Test -Level VeryVerbose
 
-    $activity = "Checking Guest access is limited to approved tenants"
-    Write-ZtProgress -Activity $activity -Status "Getting policy"
+    $activity = "Checking if domain-based allow/deny lists is configured"
+    Write-ZtProgress -Activity $activity
 
-    $result = $false
-    $testResultMarkdown = "Planned for future release."
-    $passed = $result
+    $result = Invoke-ZtGraphRequest -RelativeUri "legacy/policies" -ApiVersion beta
+    $b2BManagementPolicyDefinition = ($result | Where-Object -FilterScript {$_.Type -eq "B2BManagementPolicy"}).definition
+    $b2BManagementPolicy = ( $b2BManagementPolicyDefinition | ConvertFrom-Json).B2BManagementPolicy
+    $allowedDomains = $b2BManagementPolicy.InvitationsAllowedAndBlockedDomainsPolicy.AllowedDomains
 
+    $passed = $allowedDomains.Count -gt 0
+    if ($passed) {
+        $testResultMarkdown = "Guest access is limited to approved tenants ✅"
+    }
+    else {
+        $testResultMarkdown = "Guest access is note limited to approved tenants ❌"
+    }
 
-    Add-ZtTestResultDetail -TestId '21822' -Title "Guest access is limited to approved tenants" `
-        -UserImpact Medium -Risk Medium -ImplementationCost High `
-        -AppliesTo Identity -Tag Identity `
-        -Status $passed -Result $testResultMarkdown -SkippedBecause UnderConstruction
+    Add-ZtTestResultDetail -TestId '21822' -Title 'Checks if domain-based allow/deny lists is configured' `
+        -UserImpact Medium -Risk Medium -ImplementationCost Low `
+        -AppliesTo Identity -Tag Application `
+        -Status $passed -Result $testResultMarkdown
 }
