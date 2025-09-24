21b1eb639d6c0d21640dafde023bb9e5312c64cd
Sandeep <sandeep.p@perennialsys.com>
2025-09-24 12:41:38 +0530

    initial commit

--- src/powershell/tests/Test-Assessment.21850.ps1
+++ src/powershell/tests/Test-Assessment.21850.ps1
@@ -3,7 +3,7 @@
 
 #>
 
-function Test-Assessment-21850{
+function Test-Assessment-21850 {
     [ZtTest(
     	Category = 'Access control',
     	ImplementationCost = 'Low',
@@ -21,15 +21,53 @@ function Test-Assessment-21850{
     Write-PSFMessage '🟦 Start' -Tag Test -Level VeryVerbose
 
     $activity = "Checking Smart lockout threshold isn't greater than 10"
-    Write-ZtProgress -Activity $activity -Status "Getting policy"
+    Write-ZtProgress -Activity $activity -Status "Getting password rule settings"
 
-    $result = $false
-    $testResultMarkdown = "Planned for future release."
-    $passed = $result
+    # Get the Password Rule Settings template
+    $passwordRuleSettings = Invoke-ZtGraphRequest -RelativeUri "directorySettingTemplates/5cf42378-d67d-4f36-ba46-e8b86229381d" -ApiVersion beta
 
+    $mdInfo = ""
 
-    Add-ZtTestResultDetail -TestId '21850' -Title "Smart lockout threshold isn't greater than 10" `
-        -UserImpact Low -Risk Medium -ImplementationCost Low `
-        -AppliesTo Identity -Tag Identity `
-        -Status $passed -Result $testResultMarkdown -SkippedBecause UnderConstruction
+    if ($null -eq $passwordRuleSettings) {
+        # Test Failed - Template not found
+        $passed = $false
+        $testResultMarkdown = "Password Rule Settings template not found.%TestResult%"
+    }
+    else {
+        # Find the LockoutThreshold setting
+        $lockoutThresholdSetting = $passwordRuleSettings.values | Where-Object { $_.name -eq "LockoutThreshold" }
+
+        if ($null -eq $lockoutThresholdSetting) {
+            # Test Failed - LockoutThreshold Setting not found
+            $passed = $false
+            $testResultMarkdown = "LockoutThreshold setting not found in Password Rule Settings.%TestResult%"
+        }
+        else {
+            $lockoutThreshold = [int]$lockoutThresholdSetting.defaultValue
+
+            # Build info table
+            $mdInfo = "`n## Smart Lockout Configuration`n`n"
+            $mdInfo += "| Setting | Value |`n"
+            $mdInfo += "| :---- | :---- |`n"
+            $mdInfo += "| Lockout Threshold | $lockoutThreshold |`n"
+
+            if ($lockoutThreshold -gt 10) {
+                $passed = $true
+                $testResultMarkdown = "Smart lockout threshold is configured above 10.%TestResult%"
+            }
+            else {
+                $passed = $false
+                $testResultMarkdown = "Smart lockout threshold is set to 10 or below.%TestResult%"
+            }
+        }
+    }
+    $testResultMarkdown = $testResultMarkdown -replace "%TestResult%", $mdInfo
+
+    $params = @{
+        TestId             = '21850'
+        Status             = $passed
+        Result             = $testResultMarkdown
+    }
+
+    Add-ZtTestResultDetail @params
 }
