e1a128a58e28d21cf972f7a26c1c87df9afd9805
sandeep <sandeep.p@perennialsys.com>
2025-10-07 14:32:39 +0530

    added comma separation to multiple permissions

--- src/powershell/tests/Test-Assessment.24518.ps1
+++ src/powershell/tests/Test-Assessment.24518.ps1
@@ -116,8 +116,8 @@ function Test-Assessment-24518 {
             }
         }
 
-        $permList = if ($permDisplay) { ($permDisplay | Sort-Object -Unique) -join '<br>'} else { 'None' }
-        $classList = if ($classDisplay) { ($classDisplay | Sort-Object -Unique) -join '<br>'} else { 'Unknown' }
+        $permList = if ($permDisplay) { ($permDisplay | Sort-Object -Unique) -join ", "} else { 'None' }
+        $classList = if ($classDisplay) { ($classDisplay | Sort-Object -Unique) -join ", "} else { 'Unknown' }
 
         # Build clickable Entra portal link for the application
         $entraLink = "https://entra.microsoft.com/#view/Microsoft_AAD_IAM/StartboardApplicationsMenuBlade/~/AppAppsPreview/objectId/$($app.id)"
