fa2621be1696682e1fffe1b907b81d71693cf5dd
merill <merill@users.noreply.github.com>
2025-10-24 15:31:39 +1100

    Updated footer

--- src/powershell/private/export/Export-GraphEntityPrivilegedGroup.ps1
+++ src/powershell/private/export/Export-GraphEntityPrivilegedGroup.ps1
@@ -38,7 +38,6 @@
                 # Add the group id property to the member object
                 $member | Add-Member -MemberType NoteProperty -Name privilegedGroupId -Value $groupId -Force # Need to force to overwrite if it was cached user.
                 # Add the RoleDefinitionId property
-                Write-Host "Adding Role Definition Id: $($_.roleDefinitionId) to member $($member.displayName)"
                 $member | Add-Member -MemberType NoteProperty -Name roleDefinitionId -Value $_.roleDefinitionId -Force
                 $results.value += $member
             }
