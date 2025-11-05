1c73a464075be75c06adde75dd9ad1d3012984e5
merill <merill@users.noreply.github.com>
2025-11-05 16:58:29 +1100

    Fixed social card

--- src/react/docusaurus.config.js
+++ src/react/docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/social-card.png',
       navbar: {
         title: 'Zero Trust Workshop',
         logo: {
