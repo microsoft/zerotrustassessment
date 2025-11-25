07e181033ec870f4793b712aae727587f71cea38
Sandeep Jha <sandeep.p@perennialsys.com>
2025-11-26 01:16:18 +0530

    handled null value for policyMigrationState (#633)
    Co-authored-by: varsha123 <varsha.mane@perennialsys.com>

--- src/powershell/tests/Test-Assessment.21803.ps1
+++ src/powershell/tests/Test-Assessment.21803.ps1
@@ -21,19 +21,24 @@ function Test-Assessment-21803 {
 
     Write-PSFMessage '🟦 Start' -Tag Test -Level VeryVerbose
 
-    $activity = "Checking Migrate from legacy MFA and SSPR policies"
-    Write-ZtProgress -Activity $activity -Status "Getting policy"
+    $activity = 'Checking Migrate from legacy MFA and SSPR policies'
+    Write-ZtProgress -Activity $activity -Status 'Getting policy'
 
-    $result = Invoke-ZtGraphRequest -RelativeUri "policies/authenticationMethodsPolicy" -ApiVersion beta
+    $result = Invoke-ZtGraphRequest -RelativeUri 'policies/authenticationMethodsPolicy' -ApiVersion beta
     if ($null -eq $result) {
-        Write-ZtProgress -Activity $activity -Status "Failed to retrieve policy"
+        Write-ZtProgress -Activity $activity -Status 'Failed to retrieve policy'
         return
     }
 
     # Check if combined security information registration is enabled in the tenant
-    if ($result.policyMigrationState -eq "migrationComplete") {
+    if ($result.policyMigrationState -eq 'migrationComplete' -or $null -eq $result.policyMigrationState) {
         $passed = $true
-        $testResultMarkdown = "Combined registration is enabled.`n`n"
+        if ($null -eq $result.policyMigrationState) {
+            $testResultMarkdown = "No legacy policies to migrate. This tenant is using modern authentication methods.`n`n"
+        }
+        else {
+            $testResultMarkdown = "Combined registration is enabled.`n`n"
+        }
     }
     else {
         $passed = $false
@@ -41,13 +46,7 @@ function Test-Assessment-21803 {
     }
 
     $params = @{
-        TestId             = '21803'
-        Title              = "Migrate from legacy MFA and SSPR policies"
-        UserImpact         = 'Medium'
-        Risk               = 'High'
-        ImplementationCost = 'Medium'
-        AppliesTo          = 'Identity'
-        Tag                = 'Identity'
+        TestId             = 21803
         Status             = $passed
         Result             = $testResultMarkdown
     }
