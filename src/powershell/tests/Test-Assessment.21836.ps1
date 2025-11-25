f0fd315e0904017c947c762ea4a5d5b3ccc5987e
Praneeth <chukka.p@perennialsys.com>
2025-11-25 22:34:05 +0530

    Fix: Update service principal links to redirect to enterprise apps in Entra portal

--- src/powershell/tests/Test-Assessment.21836.ps1
+++ src/powershell/tests/Test-Assessment.21836.ps1
@@ -26,9 +26,10 @@ function Test-Assessment-21836{
 
     # Get workload identities with privileged roles in a single query
     $sql = @"
-SELECT principalId, principalDisplayName, userPrincipalName, roleDisplayName, roleDefinitionId, privilegeType, isPrivileged, "@odata.type"
-FROM vwRole
-WHERE isPrivileged = 1 AND "@odata.type" in ('#microsoft.graph.servicePrincipal', '#microsoft.graph.managedIdentity')
+SELECT vr.principalId, vr.principalDisplayName, sp.appId, vr.userPrincipalName, vr.roleDisplayName, vr.roleDefinitionId, vr.privilegeType, vr.isPrivileged, vr."@odata.type"
+FROM vwRole vr
+LEFT JOIN ServicePrincipal sp ON vr.principalId = sp.id
+WHERE vr.isPrivileged = 1 AND vr."@odata.type" in ('#microsoft.graph.servicePrincipal', '#microsoft.graph.managedIdentity')
 "@
 
     $workloadIdentitiesWithPrivilegedRoles = @(Invoke-DatabaseQuery -Database $Database -Sql $sql)
@@ -44,10 +45,11 @@ WHERE isPrivileged = 1 AND "@odata.type" in ('#microsoft.graph.servicePrincipal'
         foreach ($assignment in $sortedAssignments) {
             $principalName = $assignment.principalDisplayName
             $principalId = $assignment.principalId
+            $principalAppId = $assignment.appId
             $roleName = $assignment.roleDisplayName
             $assignmentType = $assignment.privilegeType
 
-            $spLink = 'https://entra.microsoft.com/#view/Microsoft_AAD_IAM/ManagedAppMenuBlade/~/Overview/objectId/{0}' -f $principalId
+            $spLink = 'https://entra.microsoft.com/#view/Microsoft_AAD_IAM/ManagedAppMenuBlade/~/Overview/objectId/{0}/appId/{1}/preferredSingleSignOnMode~/null/servicePrincipalType/Application/fromNav/' -f $principalId, $principalAppId
             $testResultMarkdown += "| [$(Get-SafeMarkdown $principalName)]($spLink) | $roleName | $assignmentType |`n"
         }
         $testResultMarkdown += "`n"
