54cc85a0cb333639d5744e60bd29643f7932d43e
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2025-12-11 14:22:41 +0100

    Fixing throttling & dev experience update

--- src/powershell/scripts/extend-supportpackage.ps1
+++ src/powershell/scripts/extend-supportpackage.ps1
@@ -1,2 +1,3 @@
-﻿# Ensure that any support packages also include the statistics of the last test run
+﻿# Ensure that any support packages also include the statistics of the last export and test run
+Register-PSFSupportDataProvider -Name 'ZTA_ExportStatistics' -ScriptBlock { Get-ZtExportStatistics }
 Register-PSFSupportDataProvider -Name 'ZTA_TestStatistics' -ScriptBlock { Get-ZtTestStatistics }
