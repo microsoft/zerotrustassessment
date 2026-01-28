65075c4a41dc51bbedde2aa340262d2ba8ab3ed5
Kshitiz sharma <kshiti.p@perennialsys.com>
2026-01-28 17:13:32 +0530

    Is global scope update

--- src/powershell/tests/Test-Assessment.35018.ps1
+++ src/powershell/tests/Test-Assessment.35018.ps1
@@ -108,7 +108,7 @@ function Test-Assessment-35018 {
                 PolicyGuid                    = $policy.Guid
                 Enabled                       = $policy.Enabled
                 RequireDowngradeJustification = $requireDowngradeJustification
-                Scope                         =  $isGlobal
+                Scope                         =  if ($isGlobal) { 'Global' } else { 'Scoped' }
                 LabelsCount                   = $policy.Labels.Count
                 Workloads                     = ($workloads -join ', ')
             }
