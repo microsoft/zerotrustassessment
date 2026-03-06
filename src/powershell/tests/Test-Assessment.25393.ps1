59a2c8c8bce742fac641dba8c2f15db957668403
Manoj K <manoj.p@perennialsys.com>
2026-03-06 13:23:14 +0530

    Feedback Addressed

--- src/powershell/tests/Test-Assessment.25393.ps1
+++ src/powershell/tests/Test-Assessment.25393.ps1
@@ -38,11 +38,13 @@ function Test-Assessment-25393 {
     # Q1: Retrieve the Private Access traffic forwarding profile
     Write-ZtProgress -Activity $activity -Status 'Checking Private Access forwarding profile'
     $forwardingProfiles = $null
+    $q1Error = $null
     try {
         $forwardingProfiles = Invoke-ZtGraphRequest -RelativeUri 'networkAccess/forwardingProfiles' -ApiVersion beta
     }
     catch {
-        Write-PSFMessage "Unable to retrieve Private Access forwarding profile: $($_.Exception.Message)" -Tag Test -Level Warning
+        $q1Error = $_
+        Write-PSFMessage "Unable to retrieve Private Access forwarding profile: $($_.Exception.Message)" -Tag Test -Level Error
     }
     $privateProfile = $forwardingProfiles | Where-Object { $_.trafficForwardingType -eq 'private' } | Select-Object -First 1
     $profileState = if ($privateProfile) { $privateProfile.state } else { 'Not found' }
@@ -50,33 +52,49 @@ function Test-Assessment-25393 {
     # Q2: List all connector groups
     Write-ZtProgress -Activity $activity -Status 'Querying connector groups'
     $connectorGroups = @()
+    $q2Error = $null
     try {
         $connectorGroups = @(Invoke-ZtGraphRequest -RelativeUri 'onPremisesPublishingProfiles/applicationProxy/connectorGroups' -ApiVersion beta)
     }
     catch {
-        Write-PSFMessage "Unable to retrieve connector groups: $($_.Exception.Message)" -Tag Test -Level Warning
+        $q2Error = $_
+        Write-PSFMessage "Unable to retrieve connector groups: $($_.Exception.Message)" -Tag Test -Level Error
     }
 
-    # Q3: Find the connector group that has the Quick Access application assigned
+    # Q3: Find the Quick Access service principal by its tag, then locate its connector group
+    # The NetworkAccessQuickAccessApplication tag is on the servicePrincipal, not the application object.
     Write-ZtProgress -Activity $activity -Status 'Checking Quick Access application assignment'
+    $quickAccessSp = $null
+    $q3Error = $null
+    try {
+        $quickAccessSp = @(Invoke-ZtGraphRequest -RelativeUri "servicePrincipals?`$filter=tags/any(t:t eq 'NetworkAccessQuickAccessApplication')" -ApiVersion beta) | Select-Object -First 1
+    }
+    catch {
+        $q3Error = $_
+        Write-PSFMessage "Unable to query service principals for Quick Access tag: $($_.Exception.Message)" -Tag Test -Level Error
+    }
+
     $quickAccessGroup = $null
-    foreach ($group in $connectorGroups) {
-        $apps = @()
-        try {
-            $apps = @(Invoke-ZtGraphRequest -RelativeUri "onPremisesPublishingProfiles/applicationProxy/connectorGroups/$($group.id)/applications" -ApiVersion beta)
-        }
-        catch {
-            Write-PSFMessage "Unable to list applications for connector group $($group.name): $($_.Exception.Message)" -Tag Test -Level Warning
-            continue
-        }
-        if ($apps | Where-Object { @($_.tags) -contains 'NetworkAccessQuickAccessApplication' }) {
-            $quickAccessGroup = $group
-            break
+    if ($quickAccessSp) {
+        foreach ($group in $connectorGroups) {
+            $apps = @()
+            try {
+                $apps = @(Invoke-ZtGraphRequest -RelativeUri "onPremisesPublishingProfiles/applicationProxy/connectorGroups/$($group.id)/applications" -ApiVersion beta)
+            }
+            catch {
+                Write-PSFMessage "Unable to list applications for connector group $($group.name): $($_.Exception.Message)" -Tag Test -Level Warning
+                continue
+            }
+            if ($apps | Where-Object { $_.appId -eq $quickAccessSp.appId }) {
+                $quickAccessGroup = $group
+                break
+            }
         }
     }
 
     # Q4: List connectors in the Quick Access connector group
     $connectors = @()
+    $q4Error = $null
     $activeConnectorCount = 0
     if ($quickAccessGroup) {
         Write-ZtProgress -Activity $activity -Status 'Checking connectors in Quick Access connector group'
@@ -84,7 +102,8 @@ function Test-Assessment-25393 {
             $connectors = @(Invoke-ZtGraphRequest -RelativeUri "onPremisesPublishingProfiles/applicationProxy/connectorGroups/$($quickAccessGroup.id)/members" -ApiVersion beta)
         }
         catch {
-            Write-PSFMessage "Unable to list connectors for group $($quickAccessGroup.name): $($_.Exception.Message)" -Tag Test -Level Warning
+            $q4Error = $_
+            Write-PSFMessage "Unable to list connectors for group $($quickAccessGroup.name): $($_.Exception.Message)" -Tag Test -Level Error
         }
         $activeConnectorCount = @($connectors | Where-Object { $_.status -eq 'active' }).Count
     }
@@ -94,12 +113,28 @@ function Test-Assessment-25393 {
     #region Assessment Logic
 
     $passed = $false
+    $apiError = $null
 
-    if ($profileState -ne 'enabled') {
+    if ($q1Error) {
+        $apiError = "Unable to determine Quick Access status. An error occurred while retrieving the Private Access forwarding profile: $q1Error"
+    }
+    elseif ($q2Error) {
+        $apiError = "Unable to determine Quick Access status. An error occurred while retrieving connector groups: $q2Error"
+    }
+    elseif ($q3Error) {
+        $apiError = "Unable to determine Quick Access status. An error occurred while querying for the Quick Access service principal: $q3Error"
+    }
+    elseif ($q4Error) {
+        $apiError = "Unable to determine Quick Access status. An error occurred while retrieving connectors: $q4Error"
+    }
+    elseif ($profileState -ne 'enabled') {
         Write-PSFMessage "Q1 Fail: Private Access forwarding profile state is '$profileState'" -Tag Test -Level VeryVerbose
     }
     elseif ($connectorGroups.Count -eq 0) {
         Write-PSFMessage 'Q2 Fail: No connector groups found' -Tag Test -Level VeryVerbose
+    }
+    elseif ($null -eq $quickAccessSp) {
+        Write-PSFMessage 'Q3 Fail: No service principal with the NetworkAccessQuickAccessApplication tag found' -Tag Test -Level VeryVerbose
     }
     elseif ($null -eq $quickAccessGroup) {
         Write-PSFMessage 'Q3 Fail: No connector group has the Quick Access application assigned' -Tag Test -Level VeryVerbose
@@ -111,7 +146,10 @@ function Test-Assessment-25393 {
         $passed = $true
     }
 
-    $testResultMarkdown = if ($passed) {
+    $testResultMarkdown = if ($apiError) {
+        $apiError
+    }
+    elseif ($passed) {
         "Quick Access is bound to a connector group with at least one active connector, and the Private Access traffic forwarding profile is enabled.`n`n%TestResult%"
     }
     else {
@@ -126,7 +164,6 @@ function Test-Assessment-25393 {
 
     $portalLink = 'https://entra.microsoft.com/#view/Microsoft_AAD_IAM/QuickAccessMenuBlade/~/GlobalSecureAccess'
     $groupName  = if ($quickAccessGroup) { Get-SafeMarkdown -Text $quickAccessGroup.name } else { 'N/A' }
-    $groupId    = if ($quickAccessGroup) { $quickAccessGroup.id } else { 'N/A' }
     $qaAssigned = if ($quickAccessGroup) { 'Yes' } else { 'No' }
 
     $mdInfo += @"
@@ -137,7 +174,6 @@ function Test-Assessment-25393 {
 | :--- | :--- |
 | Private Access Profile State | $profileState |
 | Connector Group Name | $groupName |
-| Connector Group ID | $groupId |
 | Quick Access App Assigned | $qaAssigned |
 
 "@
