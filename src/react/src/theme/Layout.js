d9c67ce3256e02da59d8b619c1bdd98001da7e30
Tyler Chan <97770008+tylchan@users.noreply.github.com>
2026-03-25 11:50:55 -0700

    Update Layout.js

--- src/react/src/theme/Layout.js
+++ src/react/src/theme/Layout.js
@@ -1,7 +1,13 @@
 // src/theme/Layout.js
 import React from 'react';
 import OriginalLayout from '@theme-original/Layout';
+import CookieConsent from '../components/Consent/CookieConsent';
 
 export default function Layout(props) {
-    return <OriginalLayout {...props} />;
+    return (
+        <>
+            <OriginalLayout {...props} />
+            <CookieConsent />
+        </>
+    );
 }
