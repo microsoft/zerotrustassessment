a67fb35fb2d69a5cb4457ee80904764f0447581f
Ashwini Karke <ashwini.karke@perennialsys.com>
2026-08-14 10:07:08 +0530

    removed min lic

--- src/powershell/tests/Test-Assessment.25412.ps1
+++ src/powershell/tests/Test-Assessment.25412.ps1
@@ -19,7 +19,6 @@ function Test-Assessment-25412 {
         Category = 'Global Secure Access',
         ImplementationCost = 'Medium',
         Service = ('Graph'),
-        MinimumLicense = ('Entra_Premium_Internet_Access'),
         CompatibleLicense = ('Entra_Premium_Internet_Access'),
         Pillar = 'Network',
         RiskLevel = 'High',
